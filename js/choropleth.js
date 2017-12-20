a12b73db49a1f0536aea431cd17aa646b2b64e9f
apatronl <ale7feb@hotmail.com>
2017-12-19 23:21:43 -0600

    Added hover interaction to choropleth

--- js/choropleth.js
+++ js/choropleth.js
@@ -5,6 +5,16 @@ var mapHeight = +map.attr('height');
 var atlLatLng = new L.LatLng(31.7917, -3.0926);
 var myMap = L.map('map').setView(atlLatLng, 1);
 
+var info = L.control();
+info.onAdd = function(map) {
+    this._div = L.DomUtil.create('div', 'info');
+    this.update();
+    return this._div;
+}
+
+info.update = updateInfo;
+info.addTo(myMap);
+
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 5,
@@ -18,13 +28,14 @@ var svgLayer = L.svg();
 svgLayer.addTo(myMap);
 
 var lowColor = '#f2f3f4';
-var highColor = '#3182bd';
+var highColor = '#005489';
 
 d3.queue()
     .defer(d3.csv, './data/2017.csv', function(row) {
         return {
             country: row.Country,
-            score: +(+row['Happiness Score']).toFixed(3)
+            score: +(+row['Happiness Score']).toFixed(3),
+            rank: +row['Happiness Rank']
         };
     })
     .defer(d3.json, './json/countries.geo.json')
@@ -44,18 +55,23 @@ function draw(error, data, countries) {
 
     countriesDict = {};
     for (var i = 0; i < data.length; i++) {
-        countriesDict[data[i].country] = data[i].score;
+        countriesDict[data[i].country] = {
+            score: data[i].score,
+            rank: data[i].rank
+        };
     }
-    countriesLayer = L.geoJson(countries, {style: style});
+    countriesLayer = L.geoJson(countries, {
+        style: style,
+        onEachFeature: onEachFeature
+    });
     countriesLayer.addTo(myMap);
 }
 
 function style(feature) {
     var country = feature.properties.name;
-    var score = countriesDict[country];
-    if (score) {
+    if (countriesDict[country]) {
         return {
-           fillColor: colorScale(score),
+           fillColor: colorScale(countriesDict[country].score),
            weight: 1,
            opacity: 1,
            color: 'white',
@@ -71,4 +87,54 @@ function style(feature) {
        dashArray: '0',
        fillOpacity: 0.9
    };
+}
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 3.5,
+        color: '#fff',
+        dashArray: '',
+        fillOpacity: 1
+    });
+
+    info.update(layer.feature.properties);
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+}
+
+function resetHighlight(e) {
+    countriesLayer.resetStyle(e.target);
+    info.update();
+}
+
+function zoomToFeature(e) {
+    myMap.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+function updateInfo(props) {
+    if (!props) {
+        this._div.innerHTML = '<h4>2017 World Happiness Report</h4>' + 'Hover over a country';
+        return;
+    }
+    var country = props.name;
+    if (countriesDict[country]) {
+        this._div.innerHTML = '<h4>2017 World Happiness Report</h4>' +
+            '<b>' + props.name + '</b><br/>' + 'Happiness Score: ' + countriesDict[country].score
+            + '<br/> Rank: ' + countriesDict[country].rank;
+    } else {
+        this._div.innerHTML = '<h4>2017 World Happiness Report</h4>' +
+            '<b>' + props.name + '</b><br/>' + 'Country not ranked';
+    }
 }
