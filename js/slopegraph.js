895991229753a61f37a9c04b06bf7b24c519709c
apatronl <ale7feb@hotmail.com>
2017-12-20 18:03:19 -0600

    Changed slope graph to only show top 10 countries

--- js/slopegraph.js
+++ js/slopegraph.js
@@ -3,14 +3,14 @@ var slopeGraphSVG = d3.select('#ranks_slope_graph svg');
 var svgWidth = +slopeGraphSVG.attr('width');
 var svgHeight = +slopeGraphSVG.attr('height');
 
-// var padding = {t: 40, r: 50, b: 40, l: 50};
+var margin = {t: 40, r: 100, b: 40, l: 100};
 
-var graphWidth = svgWidth - padding.l - padding.r;
-var graphHeight = svgHeight - padding.t - padding.b;
+var graphWidth = svgWidth - margin.l - margin.r;
+var graphHeight = svgHeight - margin.t - margin.b;
 
-var x1 = padding.l;
-var x2 = graphWidth / 2;
-var x3 = graphWidth;
+var x1 = margin.l;
+var x2 = margin.l + graphWidth / 2;
+var x3 = graphWidth + margin.l;
 
 var years = [2015, 2016, 2017];
 var stroke = 1.8;
@@ -39,7 +39,8 @@ d3.csv('./data/yearlyData.csv',
             .entries(dataset);
 
         data = [];
-        for (var i = 0; i < dataByCountry.length; i++) {
+        // for (var i = 0; i < dataByCountry.length; i++) {
+        for (var i = 0; i < 10; i++) {
             countryData = dataByCountry[i];
             dict = { country: countryData.key };
             for (var j = 0; j < countryData.values.length; j++) {
@@ -53,11 +54,9 @@ d3.csv('./data/yearlyData.csv',
     });
 
 function drawSlopeGraph() {
-    var padding = {t: 40, r: 50, b: 40, l: 50};
-
     var scale = d3.scaleLinear()
-        .domain([1, 158])
-        .range([0, graphHeight]);
+        .domain([1, 10])
+        .range([margin.t, graphHeight]);
 
     var lines = slopeGraphSVG.selectAll('line')
         .data(data);
@@ -68,13 +67,13 @@ function drawSlopeGraph() {
         .attr('x2', x2)
         .attr('y1', function(d, i) {
             if (d['2015']) {
-                return padding.t + scale(d['2015']);
+                return scale(d['2015']);
             }
             return 0;
         })
         .attr('y2', function(d, i) {
             if (d['2016']) {
-                return padding.t + scale(d['2016']);
+                return scale(d['2016']);
             }
             return 0;
         })
@@ -87,24 +86,26 @@ function drawSlopeGraph() {
         .attr('x2', x3)
         .attr('y1', function(d, i) {
             if (d['2016']) {
-                return padding.t + scale(d['2016']);
+                return scale(d['2016']);
             }
             return 0;
         })
         .attr('y2', function(d, i) {
             if (d['2017']) {
-                return padding.t + scale(d['2017']);
+                return scale(d['2017']);
             }
             return 0;
         })
         .style("stroke", "black")
         .attr("stroke-width", stroke);
 
-    // i = padding.t;
-    //
-    // lines.enter()
-    //     .append('line')
-    //     .attr('x1', x2)
-    //     .attr('x2', x3);
-        // .attr(y1);
+    lines.enter()
+        .append('text')
+        .text(function(d) {
+            return d.country;
+        })
+        .attr('text-anchor', 'end')
+        .attr('transform', function(d) {
+            return 'translate(' + [x1/1.06, scale(d['2015'])] + ')';
+        });
 }
